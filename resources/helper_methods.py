ade614f11bdbecdb3b6c8aac0caea21924bfe915
Nicole Begnoche <29169568+nicolebegnoche@users.noreply.github.com>
2023-10-30 00:51:39 -0600

    TestQuitMsg is done; working on TestCreatePath

--- resources/helper_methods.py
+++ resources/helper_methods.py
@@ -1,58 +1,76 @@
 import os
 import json
 import requests
-
+import shutil
 
 def quit_msg(msg=None):
-    if msg: print(f'Quit msg: {msg}')
+    """ Print the msg (if any), then quit."""
+    if msg: print(f'Quit msg: {str(msg)}')
     quit()
 
 
-# def path_exists(path, doesnt_exist=None, msg=None):
-#     if os.path.exists(path): return True
-#     match doesnt_exist:
-#         case 'create': create_path(path)
-#         case 'quit': quit_msg(msg)            
-#         case _: return False
+def create_path(path):
+    path = str(path).strip()
+    reserved_chars =  r'<>:"\|?*'
 
-# def create_path(path):
-#     # quit if empty
-#     if not path: quit_msg(f'Can\'t create empty path {path}')
+    if not path:
+        quit_msg(f'Can\'t create empty path {path}')
 
-#     # split filename from directories
-#     folders = path.split('/')
-#     file = folders[-1] if '.' in folders[-1] else None
-#     folders = '/'.join(folders[:-1]) if file else '/'.join(folders)
+    if any(i in reserved_chars for i in path):
+        quit_msg(f'Path cannot contain {reserved_chars}.\n(Received {path})')
 
-#     # create directories and/or file
-#     if folders and not os.path.exists(folders): os.makedirs(folders)
-#     if file and not os.path.exists(path): open(path, 'w')
+    if (cwd := os.getcwd()) not in (abs := os.path.abspath(path)):
+        quit_msg(f'Path must be within "{cwd}".\nPath "{path}" directs to "{abs}"')
+
+    # split filename from directories
+    folders = path.split('/')
+    file = folders[-1] if '.' in folders[-1] else None
+    folders = '/'.join(folders[:-1]) if file else '/'.join(folders)
+
+    # create directories and/or file
+    if folders and not os.path.exists(folders): os.makedirs(folders)
+    if file and not os.path.exists(path): open(path, 'w')
 
 
-# ''' JSON Methods '''
-# def json_read_from_file(filepath):
-#     path_exists(filepath, f"{filepath} doesn't exist.")
-#     try:
-#         with open(filepath) as file:
-#             return json.load(file)
-#     except Exception as error:
-#         quit_msg("Failed to read from file.", error)
 
-# def json_write_to_file(data, filepath, indent=1):
-#     try:
-#         with open(filepath, 'w+') as file:
-#             json.dump(data, file, indent=indent)
-#             file.close()
-#     except Exception as error:
-#         quit_msg("Failed to write to file", error)
+def path_exists(path, doesnt_exist=None, msg=None):
+    if os.path.exists(path): return True
+    match doesnt_exist:
+        case 'create': create_path(path)
+        case 'quit': quit_msg(msg)            
+        case _: return False
 
-# ''' HTTP '''
-# def get_request(URL=None, HEADERS=None):
-#     # True if status code is between 200 and 400
-#     if not (r := requests.get(URL, headers=HEADERS)):
-#         msg =  f'Error accessing {r.url}\n'
-#         msg += f'{r.status_code}: {r.reason}'
-#         quit_msg(msg)
-#     else:
-#         return r.json()
 
+""" JSON Methods """
+def json_read_from_file(filepath):
+    path_exists(filepath, f"{filepath} doesn't exist.")
+    try:
+        with open(filepath) as file:
+            return json.load(file)
+    except Exception as error:
+        quit_msg("Failed to read from file.", error)
+
+def json_write_to_file(data, filepath, indent=1):
+    try:
+        with open(filepath, 'w+') as file:
+            json.dump(data, file, indent=indent)
+            file.close()
+    except Exception as error:
+        quit_msg("Failed to write to file", error)
+
+""" HTTP """
+def get_request(URL=None, HEADERS=None):
+    # True if status code is between 200 and 400
+    if not (r := requests.get(URL, headers=HEADERS)):
+        msg =  f'Error accessing {r.url}\n'
+        msg += f'{r.status_code}: {r.reason}'
+        quit_msg(msg)
+    else:
+        return r.json()
+
+
+if __name__ == "__main__":
+    new_path = ("..")
+    
+    create_path(new_path)
+    print('Process Finished.')
--- resources/test_helper_methods.py
+++ resources/test_helper_methods.py
@@ -1,22 +1,107 @@
 import pytest
-from helper_methods import *
+import string
+import re
+import itertools
 
-class TestQuitMsg:
-    msgs = ['Message A', '0']
-    symbols = ['`~!@#$%^&*\\|()-_=+[]{};:\'",<.>/?']
-    numbers = ['0123', 4567, 8+9, 0o123]
-    false_msgs = ['', 0, False, None]
+from resources.helper_methods import *
 
-    # messages that should print
-    @pytest.mark.parametrize('msg', (msgs + symbols + numbers))
-    def test_prints_message(self, capsys, msg):
+reserved_chars =  r'<>:"\|?*'
+
+# @pytest.mark.skip(reason='testing_other_functions')
+# class TestQuitMsg:
+#     # header    quit_msg(msg=None)
+#     # behavior  prints the message (if there is one), then quits
+#     # return    none
+
+#     msgs = ["My message", string.printable, string.punctuation]
+#     system = [BaseException, Exception, SystemError, SyntaxError, SystemExit]
+#     numbers = ['0123', 4567, 8+9, 0o123]
+#     false_msgs = ['', 0, False, None]
+
+#     # messages that should print
+#     @pytest.mark.parametrize('msg', (msgs + numbers + system))
+#     def test_print_message(self, capsys, msg):
+#         with pytest.raises(SystemExit):
+#             quit_msg(msg)
+#         assert str(msg) in capsys.readouterr().out
+
+#     # messages that should not print
+#     @pytest.mark.parametrize('msg', false_msgs)
+#     def test_skip_message(self, capsys, msg):
+#         with pytest.raises(SystemExit):
+#             quit_msg(msg)
+#         assert not capsys.readouterr().out 
+
+
+# @pytest.mark.skip(reason="in progress")
+# TODO: implement pytest temporary directory fixtures
+class TestCreatePath:
+    # header:   def create_path(path)
+    # behavior: create a file or folder according to user input
+    # return:   True if path is created (pathor already existed); False
+
+    tree = '___test/A/B/'     # keep trailing / for regex match
+    setup_filenames = ['file.txt']  # needs to be in a list for itertools
+
+
+    # valid_chars = ''.join((a for a in string.printable if a not in TestCreatePath.reserved_characters))
+    # valid_chars = ''.join([a for a in string.printable if a not in TestCreatePath.reserved_characters])
+
+    # @pytest.fixture
+    # def test_tmp_dir(self):
+    #     tree = __class__().tree
+    #     filenames = __class__().filenames
+
+    #     # remove top level folder if it exists
+    #     if os.path.exists(folder := tree.split('/')[0]): shutil.rmtree(folder)
+
+    #     # generate list of files
+    #     delimiter_indices = (x.span()[0] for x in re.finditer('/', tree))
+    #     folders = [tree[0:i+1] for i in delimiter_indices]
+    #     files = [os.path.join(x,y) for (x,y) in itertools.product(folders, filenames)]
+
+    #     # create folders and files
+    #     os.makedirs(tree)
+    #     for file in files:
+    #         if not os.path.exists(file): open(file, 'w')
+
+    #     # confirm creation
+    #     assert os.path.exists(tree)
+    #     for file in files: assert os.path.exists(file)
+
+    @pytest.mark.parametrize('path', reserved_chars)
+    def test_improper_path_name(self, path):
         with pytest.raises(SystemExit):
-            quit_msg(msg)
-        assert str(msg) in capsys.readouterr().out
+            create_path(f"a{path}b")
 
-    # messages that should not print
-    @pytest.mark.parametrize('msg', false_msgs)
-    def test_prints_message(self, capsys, msg):
+    @pytest.mark.parametrize('path', ['', ' ', '  '])
+    def test_empty_path_name(self, path):
         with pytest.raises(SystemExit):
-            quit_msg(msg)
-        assert not capsys.readouterr().out
+            create_path(path)       
+
+    bad_paths = [
+        '..',
+        'a/b/../../..'
+    ]
+
+#     def test_path_goes_above_root_directory(path):
+#         # quit with message if path attempts to go above the root directory
+#         assert False
+
+#     def test_path_already_exists(path):
+#         # return True if the file or folder already exists
+#         assert False
+ 
+#     def test_path_doesnt_exist(path):
+#        # create the new file or folder and return True
+#         assert False
+
+#     def test_some_folders_exist(path):
+#         # create the missing folders (and file, if appropriate)
+#         assert False
+
+
+
+    # def test_rm_dir(self):
+    #     top_folder = __class__().tree.split('/')[0]
+    #     shutil.rmtree(top_folder)
